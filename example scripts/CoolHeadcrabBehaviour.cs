c7d262bed77dd993b377943e16d03ab358f51417
Ajay <87275549+4jayXD@users.noreply.github.com>
2024-10-01 21:19:35 +0100

    Update CoolHeadcrabBehaviour.cs

--- example scripts/CoolHeadcrabBehaviour.cs
+++ example scripts/CoolHeadcrabBehaviour.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using HeadcrabMod;
 
-namespace CoolMod 
+namespace Example 
 {
   public class CoolHeadcrabBehaviour : HeadcrabBehaviourBase //Make sure name of class matches file. & extend off of the HeadcrabBehaviourBase for a headcrab.
   {
-    public override void Start() {}
+    public override void Start() 
+    {
+      
+    }
 
-    public override void Update() {}
+    public override void Update() 
+    {
+      
+    }
+
+    public override void Attach(GameObject host) // host = person being fucked up. Also Attaches the crab to the person.
+    {
+      
+    }
   }
 }
