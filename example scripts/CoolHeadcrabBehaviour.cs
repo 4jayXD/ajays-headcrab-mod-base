d7af59afcc2d00a6658a081ff427741358738baf
Ajay <87275549+4jayXD@users.noreply.github.com>
2024-10-01 21:15:37 +0100

    Create CoolHeadcrabBehaviour.cs

---
+++ example scripts/CoolHeadcrabBehaviour.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using HeadcrabMod;
+
+namespace CoolMod 
+{
+  public class CoolHeadcrabBehaviour : HeadcrabBehaviourBase //Make sure name of class matches file. & extend off of the HeadcrabBehaviourBase for a headcrab.
+  {
+    public override void Start() {}
+
+    public override void Update() {}
+  }
+}
