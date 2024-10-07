50c7bdfe2cbc03782c3746de1ab9b41d4eb43d96
Ajay <87275549+4jayXD@users.noreply.github.com>
2024-10-07 08:31:34 +0100

    Update CoolZombieBehaviour.cs
    Writing out template 4 zombie but ran out of time

---
+++ example scripts/CoolZombieBehaviour.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using HeadcrabMod;
+
+public class CoolZombieBehaviour : ZombieBehaviourBase 
+{
+    public override void GetAssets() //set up asset variables here.
+    {
+        
+    }
+
+    public override void Start() 
+    {
+        
+    }
+}
