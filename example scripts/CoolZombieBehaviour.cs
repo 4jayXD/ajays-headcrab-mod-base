30a60839e39ce2eb2c81fc62b2286401adb09287
Ajay <87275549+4jayXD@users.noreply.github.com>
2024-10-10 08:11:06 +0100

    Update CoolZombieBehaviour.cs

--- example scripts/CoolZombieBehaviour.cs
+++ example scripts/CoolZombieBehaviour.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
 using HeadcrabMod;
 
-public class CoolZombieBehaviour : ZombieBehaviourBase 
+namespace Example
 {
-    public override void GetAssets() //set up asset variables here.
+    public class CoolZombieBehaviour : ZombieBehaviourBase 
     {
+        public override void GetAssets() //set up asset variables here.
+        {
         
-    }
+        }
 
-    public override void Start() 
-    {
-        base.Start();
-    }
+        public override void Start() 
+        {
+            base.Start();
+        }
 
-    public override void Update()
-    {
-        base.Update();
+        public override void Update()
+        {
+            base.Update();
+        }
     }
 }
